8b2b7354045ecd1a4c123b4c62c403d9683e473c
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-11-18 14:53:36 -0500

    renamed texture shader copies
    since those shaders are rendering normal colors, not textures

---
+++ mammoth-demo/Project/shaders/normal_frag.glsl
@@ -0,0 +1,17 @@
+#version 430 core
+
+uniform sampler2D uni_texture;
+
+// Interpolated values from the vertex shaders.
+in vec3 v2f_normal;
+in vec2 v2f_uv;
+in vec4 v2f_color;
+
+// Ouput data.
+out vec4 out_color;
+
+
+
+void main() {
+    out_color = vec4(v2f_normal, 1);
+}
