feb67984f97bfedf2937669142b7e48f06626b59
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-11-18 14:58:33 -0500

    renamed texture shader files

--- mammoth-demo/Project/shaders/texture_frag.glsl
+++ mammoth-demo/Project/shaders/texture_frag.glsl
@@ -14,9 +14,9 @@ out vec4 out_color;
 
 void main() {
     // Output color = color of the texture at the specified UV.
-    out_color = texture(uni_texture, v2f_uv) * v2f_color * vec4(v2f_normal, 1);
+    out_color = texture(uni_texture, v2f_uv) * v2f_color;// * vec4(v2f_normal, 1);
     //out_color = vec4(v2f_normal, 1);
-    //out_color.a = texture(uni_texture, v2f_uv).a;
+    //out_color.a = 0.4;
     //if (texture(uni_texture, v2f_uv).a == 0) discard;
 	// Turn transparent parts of a texture pink.
 	//if (texture(uni_texture, v2f_uv).a < 1) out_color = vec4(1,0,1,1);
