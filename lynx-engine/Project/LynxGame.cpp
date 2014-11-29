33afa19a3daa266ca9ba765707dee6758e640dcb
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-11-29 16:46:18 -0500

    made LynxGame a little cleaner

--- lynx-engine/Project/LynxGame.cpp
+++ lynx-engine/Project/LynxGame.cpp
@@ -16,70 +16,45 @@ using namespace lynx;
 
 
 
-LynxGame::LynxGame() {}
+LynxGame::LynxGame() {
+	hasQuit = false;
+}
 
 
 LynxGame::~LynxGame() {
-	if (window.isOpen()) quit();
+	quit();
 }
 
 
-void LynxGame::createWindow(int width, int height, std::string windowName, bool isFullscreen) {
-	printf("game start \n");
-	/**
-	 * DEFAULT SFML WINDOW SETTINGS
-	 * sf::ContextSettings settings;
-	 * settings.depthBits = 24;
-	 * settings.stencilBits = 0;
-	 * settings.antialiasingLevel = 0;
-	 * settings.majorVersion = 4;
-	 * settings.minorVersion = 4;
-	*/
-	GLuint style = sf::Style::Default;
-	if (isFullscreen) sf::Style::Fullscreen;
-	window.create(sf::VideoMode(width, height), windowName.c_str(), style);
-	window.setVerticalSyncEnabled(true);
-	// Limit the framerate to 60 frames per second.
-	window.setFramerateLimit(60);
-
-	/*
-		GLEW has a problem with core contexts. It calls
-		glGetString(GL_EXTENSIONS), which causes GL_INVALID_ENUM
-		on GL3.2 core context as soon as glewInit() is called.
-		It also doesn't fetch the function pointers. The solution
-		is for GLEW to use glGetStringi instead. The current version
-		of GLEW is 1.10.0 but they still haven't correct it.
-		The glError() is #1280.
-	*/
-	glewExperimental = GL_TRUE; // Needed in core profile.
-	GLenum err = glewInit();
-	if (err != GLEW_OK) {
-		//Problem: glewInit failed, something is seriously wrong.
-		printf("glewInit failed.");
-	}
+void LynxGame::setup(int windowWidth, int windowHeight, std::string windowName, bool isFullscreen) {
+	// TODO parse a settings manager
+	window.init(windowWidth, windowHeight, windowName, isFullscreen);
+	init();
+	beginLoop();
 }
 
 
-void LynxGame::start() {
-	// Enable OpenGL stuff.
-	glEnable(GL_DEPTH_TEST);
-	//glEnable(GL_CULL_FACE);
-	// Accept fragment if it's closer to the camera than the former one.
-	glDepthFunc(GL_LESS);
-
+void LynxGame::beginLoop() {
 	while (window.isOpen()) {
 		// Check all the window's events that were triggered since the last iteration of the loop.
 		sf::Event event;
 		while (window.pollEvent(event)) {
-			// Close event occured.
-			if (event.type == sf::Event::Closed) {
+			switch (event.type) {
+			case sf::Event::Closed:
+				onClose();
 				quit();
-			} // Window resize occured.
-			else if (event.type == sf::Event::Resized) {
-				glViewport(0, 0, event.size.width, event.size.height);
+				break;
+			case sf::Event::Resized:
+				onResize(event.size.width, event.size.height);
+				break;
+			case sf::Event::KeyPressed:
+				onKeyDown(event.key.code);
+				break;
+			case sf::Event::KeyReleased:
+				onKeyUp(event.key.code);
+				break;
 			}
 		}
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		update();
 		render();
 		// End the current frame and display its contents on screen (internally swaps the front and back buffers).
@@ -89,6 +64,8 @@ void LynxGame::start() {
 
 
 void LynxGame::quit() {
-	printf("game quit");
-	window.close();
+	if (!hasQuit) {
+		hasQuit = true;
+		if (window.isOpen()) window.close();
+	}
 }
--- lynx-engine/Project/LynxGame.h
+++ lynx-engine/Project/LynxGame.h
@@ -1,32 +1,32 @@
 #pragma once
-#include <GL/glew.h>
-#include <SFML/Window.hpp>
-
 // TODO: get rid of printf statements and log to my logmanager instead.
 #include <iostream>
+#include "Window.h"
 
 namespace lynx {
+	using KeyCode = sf::Keyboard::Key;
 	class LynxGame {
 	public:
-		sf::Window window;
-
 		LynxGame();
+
 
 		virtual ~LynxGame();
 
 
-		void createWindow(int width, int height, std::string windowName, bool isFullscreen = false);
-
-
-		void start();
-
-
+		void setup(int windowWidth, int windowHeight, std::string windowName, bool isFullscreen = false);
 		void quit();
 
 
+		virtual void onClose() = 0;
+		virtual void onKeyDown(KeyCode key) = 0;
+		virtual void onKeyUp(KeyCode key) = 0;
+		virtual void onResize(int width, int height) = 0;
+		virtual void init() = 0;
 		virtual void render() = 0;
-
-
 		virtual void update() = 0;
+	private:
+		Window window;
+		bool hasQuit;
+		void beginLoop();
 	};
 }
