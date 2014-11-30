d64938a0194021394b09d95039d41f099cb0613b
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-11-30 01:24:31 -0500

    turned my logger into a super cool namespace thing
    Read the comments

--- lynx-engine/Project/Logger.cpp
+++ lynx-engine/Project/Logger.cpp
@@ -1,56 +1,109 @@
 #include "Logger.h"
-using namespace lynx;
-
-std::map<std::string, Logger*> Logger::loggers;
-
-const char* Logger::SEVERITY_LEVEL_NAME[3] = { "ERROR", "WARN", "" };
 
 
-
-Logger::Logger(std::string filename, SeverityLevel level) {
-	// Set the settings.
-	logFilePath = filename + ".log";
-	severityLevel = level;
-
-	// Make sure to close the current log file if it's open.
-	if (io.isOutOpen()) io.closeFileOut();
-
-	// Open a new file for the logger.
-	io.openFileOut(logFilePath);
-}
-
-
-Logger::~Logger() {
-	// Make sure to close the current log file if it's open.
-	if (io.isOutOpen()) io.closeFileOut();
-
-	// TODO: Delete all loggers.
-
-}
+/**-------------------------------------
+* Special Global + Anonymous Namespace
+* -------------------------------------
+* This is better than a static class layout apparently. Basically,
+* the first anonymous namespace holds global variables that no one can access except
+* the Log namespace below. This is safer than having private variables in a class
+* full of static methods I guess.
+* http://stackoverflow.com/questions/9321/how-do-you-create-a-static-class-in-c
+*/
+namespace lynx {
+	namespace Log {
+		namespace {
+			/**
+			* Strings that represent each severity level. They are defined in Logger.cpp.
+			*/
+			const char* SEVERITY_LEVEL_NAME[6] = { "TRACE", "DEBUG", "FATAL", "ERROR", "WARN", "INFO" };
 
 
-Logger& Logger::get(std::string filename, SeverityLevel level) {
-	// If the logger doesn't exist, create it.
-	if (loggers.find(filename) == loggers.end()) {
-		loggers[filename] = new Logger(filename, level);
+			/**
+			* The current level of severity used to define which log messages
+			* are sent to the log file.
+			*/
+			SeverityLevel severityLevel;
+
+
+			/**
+			 * FileIO is used to write to a log file based on logFilePath.
+			 */
+			FileIO io;
+
+
+			/**
+			 * The path to the log file.
+			 */
+			std::string logFilePath;
+
+			
+			/**
+			 * Write to the log file with a given severity level.
+			 * @param message The message to be written to the log file
+			 * @param level The level of severity for this message
+			 */
+			void write(std::string message, SeverityLevel level) {
+				if (level <= severityLevel) {
+					io.writel(SEVERITY_LEVEL_NAME[level] + std::string("> ") + message);
+				}
+			}
+		}
+
+
+		void init(std::string filename, SeverityLevel level) {
+			// Make sure to close the current log file if it's open.
+			if (io.isOutOpen()) io.closeFileOut();
+
+			// Set the settings.
+			logFilePath = filename + ".log";
+			severityLevel = level;
+
+			// Open a new file for the logger.
+			io.openFileOut(logFilePath);
+		}
+
+
+		void deinit() {
+			// Make sure to close the current log file if it's open.
+			if (io.isOutOpen()) io.closeFileOut();
+		}
+
+
+		void trace(std::string message) {
+			write(message, SEV_LEVEL_TRACE);
+		}
+
+
+		void debug(std::string message) {
+			write(message, SEV_LEVEL_DEBUG);
+		}
+
+
+		void fatal(std::string message) {
+			write(message, SEV_LEVEL_FATAL);
+		}
+
+
+		void error(std::string message) {
+			write(message, SEV_LEVEL_ERROR);
+		}
+
+
+		void warn(std::string message) {
+			write(message, SEV_LEVEL_WARN);
+		}
+
+
+		void info(std::string message) {
+			write(message, SEV_LEVEL_INFO);
+		}
 	}
-	loggers[filename]->severityLevel = level;
-	return *loggers[filename];
 }
 
 
-void Logger::log(std::string message, SeverityLevel level) {
-	if (level <= severityLevel) {
-		io.writel(SEVERITY_LEVEL_NAME[level] + std::string("> ") + message);
-	}
-}
 
 
-void Logger::warn(std::string message) {
-	log(message, SEV_LEVEL_WARN);
-}
 
 
-void Logger::error(std::string message) {
-	log(message, SEV_LEVEL_ERROR);
-}
+
--- lynx-engine/Project/Logger.h
+++ lynx-engine/Project/Logger.h
@@ -4,92 +4,78 @@
 
 namespace lynx {
 	/**
-	* The Logger allows the application to write messages to a specified log
-	* file. It can be configured to print messages greater than or equal to a specific
-	* severity level.
-	*/
-	class Logger {
-	public:
-		/*
-			The possible levels of severity for the output of messages to the log file.
-			Everything less than the specified severity will be written to the log file.
-		*/
+	 * The Logger allows the application to write messages to a specified log
+	 * file. It can be configured to print messages greater than or equal to a specific
+	 * severity level.
+	 */
+	namespace Log {
+		/**
+		 * The possible levels of severity for the output of messages to the log file.
+		 * Everything less than the specified severity will be written to the log file.
+		 */
 		enum SeverityLevel {
+			SEV_LEVEL_TRACE,
+			SEV_LEVEL_DEBUG,
+			SEV_LEVEL_FATAL,
 			SEV_LEVEL_ERROR,
 			SEV_LEVEL_WARN,
 			SEV_LEVEL_INFO
 		};
 
 
-		/*
-			Set the path for the log file and the severity level of the output.
-			@param filename The path to the log file.
-			@param level The level of severity that will define which messages are sent to the log file.
-		*/
-		Logger(std::string filename, SeverityLevel level = SEV_LEVEL_INFO);
-
-
-		~Logger();
-
-
-		/*
-			Get a specific logger instance. If it doesn't exist, it will be created.
-			@param filename The name of the log file.
-			@param level The level of severity that will define which messages are sent to the log file.
-		*/
-		static Logger& get(std::string filename, SeverityLevel level = SEV_LEVEL_INFO);
+		/**
+		 * Set the path for the log file and the severity level of the output.
+		 * @param filename The path to the log file.
+		 * @param level The level of severity that will define which messages are sent to the log file.
+		 */
+		void init(std::string filename, SeverityLevel level = SEV_LEVEL_INFO);
 		
+		
+		/**
+		 * Deinitialize by closing any open files.
+		 */
+		void deinit();
+		
+		
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_TRACE.
+		 * @param message The message to be written to the log file
+		 */
+		void trace(std::string message);
 
-		/*
-			Write to the log file with a given severity level. The default severity level
-			is SEV_LEVEL_INFO if none is specified.
-			@param message The message to be written to the log file
-			@param level The level of severity for this message
-		*/
-		void log(std::string message, SeverityLevel level = SEV_LEVEL_INFO);
+
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_DEBUG.
+		 * @param message The message to be written to the log file
+		 */
+		void debug(std::string message);
 
 
-		/*
-			Write to the log file with a severity level of SEV_LEVEL_WARN.
-			@param message The message to be written to the log file
-		*/
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_FATAL.
+		 * @param message The message to be written to the log file
+		 */
+		void fatal(std::string message);
+
+
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_ERROR.
+		 * @param message The message to be written to the log file
+		 */
+		void error(std::string message);
+
+
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_WARN.
+		 * @param message The message to be written to the log file
+		 */
 		void warn(std::string message);
 
 
-		/*
-			Write to the log file with a severity level of SEV_LEVEL_ERROR.
-			@param message The message to be written to the log file
-		*/
-		void error(std::string message);
-	private:
-		/*
-			FileIO is used to write to a log file based on logFilePath.
-		*/
-		FileIO io;
-
-
-		/*
-			The path to the log file.
-		*/
-		std::string logFilePath;
-
-
-		/*
-			The current level of severity used to define which log messages
-			are sent to the log file.
-		*/
-		SeverityLevel severityLevel;
-
-
-		/*
-			All loggers that have been created so far.
-		*/
-		static std::map<std::string, Logger*> loggers;
-
-
-		/*
-			Strings that represent each severity level. They are defined in Logger.cpp.
-		*/
-		static const char* SEVERITY_LEVEL_NAME[];
-	};
+		/**
+		 * Write to the log file with a severity level of SEV_LEVEL_INFO.
+		 * @param message The message to be written to the log file
+		 */
+		void info(std::string message);
+	}
 }
