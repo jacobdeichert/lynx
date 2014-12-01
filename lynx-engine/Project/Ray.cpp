371c556b6ed79a0972cf5b92a35f16946f759c23
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-12-01 05:19:29 -0500

    added intersects method to ray

--- lynx-engine/Project/Ray.cpp
+++ lynx-engine/Project/Ray.cpp
@@ -18,3 +18,32 @@ Ray::~Ray() {}
 glm::vec3 Ray::getPoint(float distance) {
 	return origin + direction * distance;
 }
+
+
+
+bool Ray::intersects(glm::vec3 position, float radius, float &distance) {
+	glm::vec3 rayToSphereDir;
+	float rayToSphereLength = 0.0f;
+	float intersectPoint = 0.0f;
+	float squaredPoint = 0.0f;
+
+	rayToSphereDir = position - origin;
+	rayToSphereLength = glm::dot(rayToSphereDir, rayToSphereDir);
+
+	intersectPoint = glm::dot(rayToSphereDir, direction);
+
+	if (intersectPoint < 0) {
+		return false;
+	}
+
+	squaredPoint = (radius * radius) - rayToSphereLength + (intersectPoint * intersectPoint);
+
+	if (squaredPoint < 0) {
+		return false;
+	}
+
+	if (distance) {
+		distance = intersectPoint - (float)sqrt(squaredPoint);
+	}
+	return true;
+}
--- lynx-engine/Project/Ray.h
+++ lynx-engine/Project/Ray.h
@@ -2,26 +2,28 @@
 #include <glm.hpp>
 
 namespace lynx {
-class Ray {
-public:
-	glm::vec3 origin;
+	class Ray {
+	public:
+		glm::vec3 origin;
 
-	glm::vec3 direction;
+		glm::vec3 direction;
 
-	Ray();
-	
-	
-	Ray(glm::vec3 origin, glm::vec3 direction);
-	
-	
-	~Ray();
+		Ray();
 
 
-	/*
-		Return a point that is a certain distance along the ray.
-	*/
-	glm::vec3 getPoint(float distance);
-};
+		Ray(glm::vec3 origin, glm::vec3 direction);
+
+
+		~Ray();
+
+
+		/**
+		 * Return a point that is a certain distance along the ray.
+		 */
+		glm::vec3 getPoint(float distance);
+
+		bool intersects(glm::vec3 position, float radius, float &distance);
+	};
 }
 
 
