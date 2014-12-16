74b4e17335319d060d58c0ed7d139bb1bfbee39b
Jake Deichert <jakedeichert@users.noreply.github.com>
2014-12-16 00:42:47 -0500

    added important comment about opengl errors

--- lynx-demo/Project/shaders/texture_vert.glsl
+++ lynx-demo/Project/shaders/texture_vert.glsl
@@ -2,6 +2,13 @@
 
 uniform mat4 uni_mvp;
 layout(location = 0) in vec3 in_position;
+/*
+NOTE: When an attribute or uniform (ex: in_normal) is not used
+in the shader, opengl removes it... and thus gives me an error
+for when i use glGetAttribLocation().
+Even if i pass it to the fragment shader (and still isn't used),
+then it's still gunna be removed and give the opengl error.
+*/
 layout(location = 1) in vec3 in_normal;
 layout(location = 2) in vec2 in_uv;
 layout(location = 3) in vec3 in_color;
