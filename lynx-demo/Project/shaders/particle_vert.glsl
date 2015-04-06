9f2afdab665428b8b1cc80bccdc2bbaa9045140b
Jake Deichert <jakedeichert@users.noreply.github.com>
2015-04-06 17:07:58 -0400

    Add example code to particle shader

--- lynx-demo/Project/shaders/particle_vert.glsl
+++ lynx-demo/Project/shaders/particle_vert.glsl
@@ -30,3 +30,58 @@ void main() {
     // The color of each vertex will be interpolated to produce the color of each fragment.
     v2f_color = vec4(in_color, 1);
 }
+
+
+
+/*
+// Example particle system
+
+Resources::loadShader("particle", "shaders/particle_vert.glsl", "shaders/particle_frag.glsl");
+
+// Create a mesh for the particle system.
+std::vector<GLfloat> vertices;
+std::vector<GLuint> elements;
+int numParticles = 20000;
+for (int i = 0; i < numParticles * 11; i += 11) {
+	// vertex pos    normal     uv       color
+	// 1, 1, 0,      0, 1, 0,   0, 1,    1, 1, 1
+
+	// vertex positions are zero
+	vertices.push_back(0);
+	vertices.push_back(0);
+	vertices.push_back(0);
+
+	// use random normals so that the particles travel in random directions
+	glm::vec3 r = glm::sphericalRand<float>(1.0f);
+	vertices.push_back(r.x);
+	vertices.push_back(r.y);
+	vertices.push_back(r.z);
+
+	// use uv.u as the random speed factor for each particle
+	vertices.push_back(glm::linearRand(10.0f, 100.0f));
+	vertices.push_back(0); // just set uv.v to zero because it isn't used
+
+	// give them random color values
+	vertices.push_back(glm::linearRand<float>(0, 1.0f));
+	vertices.push_back(glm::linearRand<float>(0, 1.0f));
+	vertices.push_back(glm::linearRand<float>(0, 1.0f));
+}
+
+// Create an element index for each particle.
+for (int j = 0; j < numParticles; j++) {
+	elements.push_back(j);
+}
+
+// Create the mesh.
+Resources::createMesh("particle", vertices, elements);
+
+// Set the size of GL_POINTS for the particles.
+glPointSize(0.5f);
+
+// Create the game object.
+GameObject *particles = new GameObject("particle_system");
+particles->addComponent(new Graphics(particles, Resources::getMesh("particle"), Resources::getShader("particle"), nullptr, GL_POINTS));
+particles->transform->setPosition(0, 0, 0);
+scene->add(particles);
+
+*/
